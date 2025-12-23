fc750a2ecbf21df39de4a4a019b55bfb1dfba5b9
Ryan-Kaiser-22 <kaiser.ryan.22@gmail.com>
2025-12-23 14:53:43 -0800

    Edited comments.

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ const equalButton = document.getElementById('=');
 const clearButton = document.getElementById('C');
 const operators = ['+', '-', 'x', '÷'];
 const calcContainer = document.getElementById('calc-container');
+// Set equals check flag to false to allow chaining ops for next equation.  
+// True will only be enabled after '=' button click to enable fresh number entry for new equation. 
 let lastInputWasEquals = false;
 
-// Check if equation is already in calculator.
+// Check if equation is already in window for chaining ops.
 function isCalcReady(windowStr) {
     const escapedOperators = operators.map(op => op.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')).join('|');
     const regex = new RegExp(`^\\s*?[0-9.]+\\s+(${escapedOperators})\\s+[0-9.]+[^\\s]*$`);
     return regex.test(windowStr.trim());
 }
 
-//Check if window only has number result.
+// Check if window only has single number with no ops.
 function isResult(windowStr) {
     const numberRegex = /^-?\d*\.?\d+(?:[eE][+-]?\d+)?$/;
     return numberRegex.test(windowStr.trim());
 }
 
-//Operate function handles the string in the window and the math. 
+// Operate function handles the calculation of window string via array. 
 function operate(arr) {
     const a = parseFloat(arr[0]);
     const op = arr[1];
@@ -43,25 +45,23 @@ function operate(arr) {
             break;
         }
     
-    // Limiting decimal places. Converting long number to exponential. 
+    // Limiting to 3 decimal places. If result is over 12 digits, convert to exponential. 
     const resultCheck = result.toLocaleString('en-US', {
         maximumFractionDigits: 3, 
         useGrouping: false
     });
-    
     if (resultCheck.length > 12) {
         return result.toExponential(4);
     } 
     return resultCheck
 }
 
+// Button listener section.
 allButtons.forEach(button => {
     button.addEventListener('click', (e) => {
-        // Trim for operator check. 
         const buttonText = e.target.textContent.trim(); 
-        //Trim for display check of equation. 
         let windowStr = displayWindow.textContent.trim(); 
-        //If on number or decimal click if last click was '=' or 'C', clear previous result.
+        // On number or decimal click: if previous click was '=', clear previous result.
         const isNumberOrDecimal = !isNaN(parseFloat(buttonText)) || buttonText === '.';
         if (buttonText !== '=' && buttonText !== 'C') {
             if (lastInputWasEquals && isNumberOrDecimal && isResult(displayWindow.textContent)) {
@@ -80,7 +80,7 @@ allButtons.forEach(button => {
         // Operator buttons. 
         if (operators.includes(buttonText)) {
             const newOp = buttonText;
-            // If the last input was '=', append new op. 
+            // If the last input was '=', append new op to prev result. 
             if (isResult(windowStr) && lastInputWasEquals) {
                  displayWindow.textContent = `${windowStr} ${newOp} `;
                  lastInputWasEquals = false;
@@ -92,7 +92,7 @@ allButtons.forEach(button => {
                 const calcInWindow = operate(windowArr);
                 if (calcInWindow === "SELF DESTRUCT") {
                     displayWindow.textContent = "SELF DESTRUCT";
-                    //RED ALERT MODE ACTIVE
+                    // RED ALERT MODE ACTIVE
                     calcContainer.classList.add('error');  
                     setTimeout(() => {
                         calcContainer.classList.remove('error');  
@@ -121,7 +121,7 @@ allButtons.forEach(button => {
                 const result = operate(windowArr); 
                 if (result === "SELF DESTRUCT") {
                     displayWindow.textContent = "SELF DESTRUCT";
-                    //RED ALERT MODE ACTIVE
+                    // RED ALERT MODE ACTIVE
                     calcContainer.classList.add('error');
                     setTimeout(() => {
                         calcContainer.classList.remove('error');
